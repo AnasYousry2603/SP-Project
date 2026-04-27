73f3b88ddcc5d0e86f2b434627761bb5c0c481a5
Anas Ahmed <AnasAhmad2603@gmail.com>
2026-04-27 20:51:35 +0300

    Added frontend

---
+++ Frontend/mynotes.cpp
@@ -0,0 +1,81 @@
+// Customer functions
+
+//-> log in
+//-> sign up
+//-> Chech room availaility
+//-> reserve room
+//-> add review
+//-> log out
+
+
+// Admin functions
+
+//-> log in
+//-> Add Reservation
+//-> View room reviews
+//-> Edit Reervation
+//-> log out
+
+
+
+// Program Scenario
+
+// ask whether he is an admin or a customer
+
+//--> enter username and password
+//--> then he chooses from the options available
+
+
+
+
+
+// file stream
+
+// automatic calculation of last 30 days reviews
+// automatic calculation of available rooms
+
+
+
+// structs
+
+// struct Customer
+// {
+//     string CustomerID;
+//     string Name;
+//     string Email;
+//     string UserName;
+//     string Password;
+//     string CreditCardNumber;
+//     Date StartDate;
+//     Date EndDate;
+//     int roomNumber = -1;
+// };
+
+
+// struct Review
+// {
+
+//     Date Date_valid;
+//     string Review_content = "###";
+//     int Review_id;
+//     int Room_number;
+//     bool Seen = false;
+// };
+
+
+
+// struct Admin
+// {
+//     string Admin_user_name;
+//     string Admin_password;
+//     int Admin_id;
+// };
+
+
+
+
+
+// after the admin adds the reservation by himself
+// this reservation isn't recorded
+// when trying to edit the reservation
+// it says (No active reservation found for this room number.)
